e0ff95b9689aa138456627171052a6014866de88
Mosho <moises.leon@chasacademy.se>
2025-10-13 07:58:05 +0200

    Adjustments to the structure for css and js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: MoS 
+ * @Date: 2025-10-10 19:02:49 
+ * @Last Modified by: MoS
+ * @Last Modified time: 2025-10-13 07:57:15
+ */
+
+const theSearch = document.getElementById("theSearch")
+const search = document.getElementById("search")
+
+const results = document.getElementById("results")
+
+const getMovies = () => {
+
+}
